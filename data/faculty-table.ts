dfea1f4ef4d4df7e06c87cafb16b7a7be1e1cf7f
Seton-tech01 <setonemmanuel001@gmail.com>
2026-05-12 11:45:47 +0100

    my local changes

---
+++ data/faculty-table.ts
@@ -0,0 +1,47 @@
+
+    export const FacultyTableData = [
+        {
+        sn: 1,
+        fullName: "FACULTY OF SCIENCE",
+        facultyId: "FAC001",
+        email: "contact123@gmail.com",
+        phone: "09030019355",
+        role: "STAFF",
+        lastLogin: "2026-05-01 10:30:00",
+        status: {
+            statusId: "1",
+            statusName: "ACTIVE",
+        },
+        image: "/all-images/image-pix/avatar.jpg",
+    },
+
+    {
+        sn: 2,
+        fullName: "FACULTY OF EDUCATION",
+        facultyId: "FAC002",
+        email: "contact123@gmail.com",
+        phone: "09030019355",
+        role: "STAFF",
+        lastLogin: "2026-05-01 10:30:00",
+        status: {
+            statusId: "2",
+            statusName: "INACTIVE",
+        },
+        image: "/all-images/image-pix/avatar.jpg",
+    },
+
+        {
+        sn: 3,
+        fullName: "FACULTY OF ART",
+        facultyId: "FAC003",
+        email: "contact123@gmail.com",
+        phone: "09030019355",
+        role: "STAFF",
+        lastLogin: "2026-05-01 10:30:00",
+        status: {
+            statusId: "2",
+            statusName: "INACTIVE",
+        },
+        image: "/all-images/image-pix/avatar.jpg",
+    }
+];
